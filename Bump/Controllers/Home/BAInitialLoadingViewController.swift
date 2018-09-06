51b7e36e3e6b9c6933cd6f2be32f48c0a511b27f
Tim Wong <tim@tjw.io>
2018-09-06 13:49:19 -0700

    initial animation commit

--- Bump/Controllers/Home/BAInitialLoadingViewController.swift
+++ Bump/Controllers/Home/BAInitialLoadingViewController.swift
@@ -7,11 +7,27 @@
 //
 
 import UIKit
+import Lottie
 import SnapKit
 
 class BAInitialLoadingViewController: UIViewController {
     
-    var activityIndicator = UIActivityIndicatorView(activityIndicatorStyle: .gray)
+    private struct Constants {
+        static let logoAnimation = "ciao_logo"
+    }
+    
+    let activityIndicator = UIActivityIndicatorView(activityIndicatorStyle: .gray)
+    
+    let bumpAnimation: LOTAnimationView = {
+        let animation = LOTAnimationView(name: Constants.logoAnimation)
+        animation.contentMode = .scaleAspectFit
+        animation.loopAnimation = false
+        animation.isHidden = false
+        animation.animationSpeed = 0.5
+        animation.translatesAutoresizingMaskIntoConstraints = false
+        
+        return animation
+    }()
     
     init(userId: String) {
         super.init(nibName: nil, bundle: nil)
@@ -35,17 +51,27 @@ class BAInitialLoadingViewController: UIViewController {
         
         view.backgroundColor = UIColor(hexColor: 0xFBFCFD)
         
-        activityIndicator.startAnimating()
         activityIndicator.translatesAutoresizingMaskIntoConstraints = false
         
+        bumpAnimation.play { [weak self] _ in
+            self?.activityIndicator.startAnimating()
+        }
+        
         view.addSubview(activityIndicator)
+        view.addSubview(bumpAnimation)
         
         setupConstraints()
     }
     
     private func setupConstraints() {
         activityIndicator.snp.makeConstraints { make in
+            make.bottom.equalTo(self.view).offset(-200.0)
+            make.centerX.equalTo(self.view)
+        }
+        
+        bumpAnimation.snp.makeConstraints { make in
             make.center.equalTo(self.view)
+            make.height.equalTo(356.0)
         }
     }
 }
